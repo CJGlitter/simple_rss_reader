c29f84f89e7139ed79c5544cf289d9c4c4ec17a8
Cory Davis <YellowDragon2009@gmail.com>
2024-03-07 21:34:33 -0600

    Cleanup description in #show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     split = str.split("<p>")
     return split[0]
   end
+
+  def returns_fix(str)
+    str.gsub "\n", "\r"
+  end
+
+  def cleanup(str)
+    new_str = trim_desc(str)
+    new_str = returns_fix(new_str)
+    return new_str
+  end
 end
